f5c0393033dfeabbdcdf27ddf1e508334cbb4042
Nick_Di <NickkciNDi@outlook.com>
2018-11-10 16:48:59 -0500

    played around with sql

--- createUserTable.sql
+++ createUserTable.sql
@@ -11,8 +11,19 @@ CREATE TABLE user_table (
 	PRIMARY KEY(userID)
 );
 
+ALTER TABLE user_table ADD tacos_eaten INT;
+
+ALTER TABLE user_table
+MODIFY 'userID' INT NOT NULL AUTO_INCREMENT; 
+
+UPDATE user_table SET tacos_eaten = '5' WHERE userID = '2';
+
 INSERT INTO user_table VALUES('1', 'user@example.com', 'United States', 'Georgia', '30094', 'IT', '28', 'male');
 
 INSERT INTO user_table VALUES('2', 'user2@example.com', 'United States', 'Georgia', '30094', 'Hospitality', '23', 'female');
+
+INSERT INTO user_table VALUES('3', 'user3@example.com', 'Italy', 'Naples', '98714', 'spaghetti', '99', 'female');
+
+INSERT INTO user_table (userEmail, age, gender) VALUES ('123@poop.com', '123', 'male');
 
 SELECT * FROM user_table WHERE userID = '1';
